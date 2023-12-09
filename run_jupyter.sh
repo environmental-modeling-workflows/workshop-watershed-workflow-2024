2b2dec6b32b7203105bfeb7a0e205482efd9d36b
saubhagya-gatech <saubhagya.rathore@outlook.com>
2023-12-09 11:49:30 -0500

    update run_jupyter.sh

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,1 +1,1 @@
-docker run -it --pull=missing -v $(pwd):/home/jovyan/workdir:delegated -w /home/jovyan/workdir -p 8888:8888 metsi/watershed-workflow:rc2
+docker run -it --pull=missing -v $(pwd):/home/jovyan/workdir:delegated -w /home/jovyan/workdir -p 8888:8888 metsi/watershed-workflow:ideas-allhands23
